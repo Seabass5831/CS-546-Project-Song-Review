1e8d9d37880f28996b7057715f1ac185a86de669
Zhiyao <15222848787@163.com>
2024-04-23 01:43:13 -0400

    add some methods and comments for the database collections

--- data/recommendations.js
+++ data/recommendations.js
@@ -1,15 +1,15 @@
 import helpers from "../helpers.js";
 import { recommendations } from "../config/mongoCollections.js";
 import { ObjectId } from "mongodb";
-/*
- create => creates a new recommendation => params : [userId, recommendedSongs]
-  remove => removes a recommendation => params: [userId]
-  get => get a recommendation => params: [userId]
-  update => update a recommendation => params: [userId, recommendedSongs]
-  getAll => get all recommendations for a user => params: [userId]
-*/
 
 const exportedMethods = {
+  /**
+   * Creates a new recommendation.
+   * @param {string} userId - The ID of the user.
+   * @param {string[]} recommendedSongs - The array of recommended songs.
+   * @returns {Promise<Recommendation>} The newly created recommendation.
+   * @throws {Error} If any of the required parameters are missing or invalid.
+   */
   async create([userId, recommendedSongs]) {
     try {
       helpers.requiredParams([userId, recommendedSongs]);
@@ -32,6 +32,13 @@ const exportedMethods = {
       throw err;
     }
   },
+
+  /**
+   * Removes a recommendation.
+   * @param {string} userId - The ID of the user.
+   * @returns {Promise<DeletionInfo>} The deletion information.
+   * @throws {Error} If the recommendation with the specified user ID cannot be found.
+   */
   async remove(userId) {
     try {
       helpers.requiredParams([userId]);
@@ -49,21 +56,37 @@ const exportedMethods = {
       throw err;
     }
   },
+
+  /**
+   * Retrieves a recommendation by user ID.
+   * @param {string} userId - The ID of the user.
+   * @returns {Promise<Recommendation>} The recommendation.
+   * @throws {Error} If the recommendation with the specified user ID cannot be found.
+   */
   async get(userId) {
     try {
       helpers.requiredParams([userId]);
       userId = helpers.checkId(userId, "userId");
       const recommendationCollection = await recommendations();
-      const new_recommendation = await recommendationCollection.findOne({
+      const recommendation = await recommendationCollection.findOne({
         _id: new ObjectId(userId),
       });
-      return new_recommendation;
+      return recommendation;
     } catch (err) {
       console.log(err);
       throw err;
     }
   },
-  async update([userId, recommendedSongs]) {
+
+  /**
+   * Updates a recommendation by user ID.
+   * @param {string} userId - The ID of the user.
+   * @param {string[]} recommendedSongs - The array of recommended songs.
+   * @returns {Promise<Recommendation>} The updated recommendation.
+   * @throws {Error} If any of the required parameters are missing or invalid,
+   * or if the recommendation with the specified user ID cannot be found.
+   */
+  async update(userId, recommendedSongs) {
     try {
       helpers.requiredParams([userId, recommendedSongs]);
       userId = helpers.checkId(userId, "userId");
@@ -94,19 +117,81 @@ const exportedMethods = {
       throw err;
     }
   },
+
+  /**
+   * Retrieves all recommendations for a user.
+   * @param {string} userId - The ID of the user.
+   * @returns {Promise<Recommendation[]>} The list of recommendations.
+   * @throws {Error} If the user with the specified ID cannot be found.
+   */
   async getAll(userId) {
     try {
       helpers.requiredParams([userId]);
       userId = helpers.checkId(userId, "userId");
       const recommendationCollection = await recommendations();
-      const ListOfrecommendations = await recommendationCollection
+      const listOfrecommendations = await recommendationCollection
         .find({ userId: userId })
         .toArray();
-      return ListOfrecommendations;
+      return listOfrecommendations;
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  },
+
+  /**
+   * Adds a song to a recommendation.
+   * @param {string} recommendationId - The ID of the recommendation.
+   * @param {string} songId - The ID of the song.
+   * @returns {Promise<Recommendation>} The updated recommendation.
+   * @throws {Error} If any of the required parameters are missing or invalid,
+   * or if the recommendation with the specified ID cannot be found.
+   */
+  async addSong(recommendationId, songId) {
+    try {
+      helpers.requiredParams([recommendationId, songId]);
+      recommendationId = helpers.checkId(recommendationId, "recommendationId");
+      songId = helpers.checkId(songId, "songId");
+      const recommendationCollection = await recommendations();
+      const updateInfo = await recommendationCollection.updateOne(
+        { _id: recommendationId },
+        { $push: { recommendedSongs: songId } }
+      );
+      if (!updateInfo.matchedCount && !updateInfo.modifiedCount)
+        throw new Error('Update failed');
+      return await this.get(recommendationId);
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  },
+
+  /**
+   * Removes a song from a recommendation.
+   * @param {string} recommendationId - The ID of the recommendation.
+   * @param {string} songId - The ID of the song.
+   * @returns {Promise<Recommendation>} The updated recommendation.
+   * @throws {Error} If any of the required parameters are missing or invalid,
+   * or if the recommendation with the specified ID cannot be found.
+   */
+  async removeSong(recommendationId, songId) {
+    try {
+      helpers.requiredParams([recommendationId, songId]);
+      recommendationId = helpers.checkId(recommendationId, "recommendationId");
+      songId = helpers.checkId(songId, "songId");
+      const recommendationCollection = await recommendations();
+      const updateInfo = await recommendationCollection.updateOne(
+        { _id: recommendationId },
+        { $pull: { recommendedSongs: songId } }
+      );
+      if (!updateInfo.matchedCount && !updateInfo.modifiedCount)
+        throw new Error('Update failed');
+      return await this.get(recommendationId);
     } catch (err) {
       console.log(err);
       throw err;
     }
   },
 };
+
 export default exportedMethods;
--- data/users.js
+++ data/users.js
@@ -1,23 +1,25 @@
+
 import helpers from "../helpers.js";
 import bcrypt from "bcrypt";
 import { users } from "../config/mongoCollections.js";
 import { ObjectId } from "mongodb";
-/*
- create => creates a new user => params : [firstName, lastName, email, password, listendSongs, reviewsPosted]
-  remove => removes a user => params : [email]
-  get => get a user by email => params : [email]
-  getAll => get all users
-  update => update a user => params : [userId, firstName, lastName, email, password, listendSongs, reviewsPosted]
-  */
+
+
 const exportedMethods = {
-  async create([
-    firstName,
-    lastName,
-    email,
-    password,
-    listendSongs,
-    reviewsPosted,
-  ]) {
+
+/**
+ * Creates a new user.
+ * @async
+ * @param {Object} user - The user object containing user information.
+ * @param {string} user.firstName - The first name of the user.
+ * @param {string} user.lastName - The last name of the user.
+ * @param {string} user.email - The email address of the user.
+ * @param {string} user.password - The password of the user.
+ * @param {Array} user.favoriteGenres - The favorite genres of the user.
+ * @returns {Promise<Object>} The newly created user object.
+ * @throws {Error} If any required parameter is missing or invalid.
+ */
+  async create([ firstName, lastName, email, password, favoriteGenres ]) {
     try {
       helpers.requiredParams([firstName, lastName, email, password]);
       firstName = helpers.checkString(firstName, "firstname");
@@ -26,24 +28,25 @@ const exportedMethods = {
       if (!helpers.isValidEmail(email)) {
         throw new Error("not a valid email");
       }
+      
       password = helpers.checkString(password, "password");
-      const listendSongs = [];
-      const reviewsPosted = [];
+      favoriteGenres = helpers.checkStringArray(favoriteGenres, "favoriteGenres");
 
       //handle password logic
       const saltRounds = 16;
       const salt = await bcrypt.genSalt(saltRounds);
       const hash = await bcrypt.hash(password, salt);
-      const firstNameHash = await bcrypt.hash(firstName, salt);
-      const lastNameHash = await bcrypt.hash(lastName, salt);
+
       const user = {
-        firstName: firstNameHash,
-        lastName: lastNameHash,
-        email: email,
-        password: hash,
-        listendSongs,
-        reviewsPosted,
+        firstName,
+        lastName,
+        email,
+        hashedPassword: hash,
+        favoriteGenres,
+        listenedSongs: [],
+        reviewsPosted: [],
       };
+
       const userCollection = await users();
       const newUser = await userCollection.insertOne(user);
       newUser._id = newUser.insertedId.toString();
@@ -53,6 +56,30 @@ const exportedMethods = {
       throw err;
     }
   },
+
+/**
+ * Retrieves all users.
+ * @async
+ * @returns {Promise<Array>} An array of all user objects.
+ * @throws {Error} If an error occurs while retrieving the users.
+ */
+  async getAllUsers() {
+    try {
+      const userCollection = await users();
+      return await userCollection.find({}).toArray();
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  },
+
+/**
+ * Removes a user by their ID.
+ * @async
+ * @param {string} userId - The ID of the user to be removed.
+ * @returns {Promise<Object>} The deletion information.
+ * @throws {Error} If the user with the specified ID does not exist.
+ */
   async remove(userId) {
     try {
       console.log(userId);
@@ -72,6 +99,13 @@ const exportedMethods = {
     }
   },
 
+/**
+ * Retrieves a user by their ID.
+ * @async
+ * @param {string} userId - The ID of the user to be retrieved.
+ * @returns {Promise<Object>} The user object.
+ * @throws {Error} If the user with the specified ID does not exist.
+ */
   async getUserById(userId) {
     try {
       helpers.requiredParams([userId]);
@@ -87,18 +121,63 @@ const exportedMethods = {
     }
   },
 
-  async getUserByEmail(email) {
-    try {
-      helpers.requiredParams([email]);
-      email = helpers.checkString(email, "email");
-      const userCollection = await users();
-      const user = await userCollection.findOne({ email: email });
-      return user;
-    } catch (err) {
-      console.log(err);
-      throw err;
-    }
-  },
+/**
+ * Retrieves a user by their email address.
+ * @async
+ * @param {string} email - The email address of the user to be retrieved.
+ * @returns {Promise<Object>} The user object.
+ * @throws {Error} If the user with the specified email address does not exist.
+ */
+async getUserByEmail(email) {
+  try {
+    helpers.requiredParams([email]);
+    email = helpers.checkString(email, "email");
+    const userCollection = await users();
+    const user = await userCollection.findOne({ email });
+    if (!user) throw new Error(`Could not find user with email ${email}`);
+    return user;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+},
+
+
+/**
+ * Validates a user's password.
+ * @async
+ * @param {string} userId - The ID of the user.
+ * @param {string} password - The password to be validated.
+ * @returns {Promise<boolean>} A boolean indicating whether the password is valid.
+ * @throws {Error} If any required parameter is missing or invalid.
+ */
+async validatePassword(userId, password) {
+  try {
+    helpers.requiredParams([userId, password]);
+    userId = helpers.checkId(userId, "userId");
+    password = helpers.checkString(password, "password");
+    const user = await this.getUserById(userId);
+    const match = await bcrypt.compare(password, user.hashedPassword);
+    return match;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+},
+
+/**
+ * Updates a user's information.
+ * @async
+ * @param {string} userId - The ID of the user to be updated.
+ * @param {string} firstName - The updated first name of the user.
+ * @param {string} lastName - The updated last name of the user.
+ * @param {string} email - The updated email address of the user.
+ * @param {string} password - The updated password of the user.
+ * @param {Array} listendSongs - The updated list of listened songs.
+ * @param {Array} reviewsPosted - The updated list of reviews posted by the user.
+ * @returns {Promise<Object>} The updated user object.
+ * @throws {Error} If any required parameter is missing or invalid.
+ */
   async update(
     userId,
     firstName,
