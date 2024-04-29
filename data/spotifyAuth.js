be0d5d0304aa575d6ef7f47fe0c344e7d998d068
cyoungclaus <cyoungcl@stevens.edu>
2024-04-29 15:37:20 -0400

    Added .env to hide auth codes, to be used later

--- data/spotifyAuth.js
+++ data/spotifyAuth.js
@@ -1,13 +1,27 @@
 import SpotifyWebApi from "spotify-web-api-node";
+import dotenv from "dotenv";
 /*
  getAccessToken => get an access token for spotify
   Setup spotify API
   import for use in other files */
 
+dotenv.config({ path: ".env" })
+
+/*  I will update this before the final submission,
+    we would all need to have the same .env file in our
+    branches
+
+const client_id = process.env.SPOTIFY_CLIENT_ID;
+const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+*/
+
+const client_id = '6e2899240f2d4830a467092f35b58b00';
+const client_secret = 'b9cb2ad1e6764dcba20d6e39b9d3b73a';
+
 // Set up Spotify API credentials
 const spotifyApi = new SpotifyWebApi({
-  clientId: "6e2899240f2d4830a467092f35b58b00",
-  clientSecret: "b9cb2ad1e6764dcba20d6e39b9d3b73a",
+  clientId: client_id,
+  clientSecret: client_secret,
   redirectUri: "https://localhost:3000",
 });
 
